cf7ebd7b7593227f33af4748eb114a7a8f220559
unknown <yusaskia11@gmail.com>
2026-03-17 10:44:24 +0700

    final desain to do list

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const input = document.getElementById("taskInput")
+const dateInput = document.getElementById("dateInput")
 const addBtn = document.getElementById("addBtn")
 const taskList = document.getElementById("taskList")
 
 let tasks = JSON.parse(localStorage.getItem("tasks")) || []
 
 function saveTasks(){
-
 localStorage.setItem("tasks", JSON.stringify(tasks))
+}
 
+function getStatus(date){
+if(!date) return ""
+
+const today = new Date().toISOString().split("T")[0]
+
+if(date < today){
+return "overdue"
+}else if(date === today){
+return "today"
+}
+return ""
 }
 
 function renderTasks(){
@@ -18,11 +30,22 @@ tasks.forEach((task,index)=>{
 
 const li = document.createElement("li")
 
+const statusClass = getStatus(task.date)
+
 li.innerHTML = `
 <input type="checkbox" ${task.done ? "checked" : ""}>
+
+<div>
 <span class="${task.done ? "completed" : ""}">
 ${task.text}
 </span>
+<span class="date ${statusClass}">
+${task.date ? task.date : ""}
+${statusClass === "overdue" ? " (Lewat deadline!)" : ""}
+${statusClass === "today" ? " (Hari ini!)" : ""}
+</span>
+</div>
+
 <button class="delete">Delete</button>
 `
 
@@ -30,21 +53,15 @@ const checkbox = li.querySelector("input")
 const deleteBtn = li.querySelector(".delete")
 
 checkbox.addEventListener("change",()=>{
-
 task.done = checkbox.checked
-
 saveTasks()
 renderTasks()
-
 })
 
 deleteBtn.addEventListener("click",()=>{
-
 tasks.splice(index,1)
-
 saveTasks()
 renderTasks()
-
 })
 
 taskList.appendChild(li)
@@ -56,34 +73,35 @@ taskList.appendChild(li)
 function addTask(){
 
 const text = input.value.trim()
+const date = dateInput.value
 
 if(text === "") return
 
 tasks.push({
-
 text:text,
+date:date,
 done:false
-
 })
 
 input.value=""
+dateInput.value=""
 
 saveTasks()
-
 renderTasks()
 
 }
 
 addBtn.addEventListener("click",addTask)
 
-input.addEventListener("keypress",(e)=>{
-
+/* 🔥 FIX ENTER */
+document.addEventListener("keydown",(e)=>{
 if(e.key === "Enter"){
+const active = document.activeElement
 
+if(active.id === "taskInput" || active.id === "dateInput"){
 addTask()
-
 }
-
+}
 })
 
-renderTasks() 
+renderTasks()
